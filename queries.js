69cd1789774d27778fef95db7b0a3227e9254485
Jonathan Pinlac <jonppinlac@gmail.com>
2017-05-16 16:09:00 -0400

    Connected to local db

--- queries.js
+++ queries.js
@@ -1,5 +1,39 @@
-const pool = require('./db');
+const db = require('./db');
 
-pool.query('SELECT * FROM users', function (err, result) {
-  console.log(result);
-});
+function getUsers(req, res, next) {
+    db.query('SELECT * FROM users', function (err, result) {
+        // console.log(result);
+        res.send(result.rows);
+    });
+}
+
+function getUser(req, res, next) {
+    var userId = parseInt(req.params.id);
+    db.query('SELECT * FROM users WHERE id = $1', [userId], function (err, result) {
+        if(err) {
+          return console.error('error running query', err);
+        }
+        res.status(200).send('number:', result);
+    });
+}
+
+// router.get('/users/:id', db.getUser);
+// router.post('/users', db.createUser);
+// router.put('/users', db.updateUser);
+// router.delete('/users/:id', db.deleteUser);
+
+
+// router.get('/statuses', db.getStatuses);
+// router.get('/statuses/:id', db.getStatus);
+// router.post('/statuses', db.createStatus);
+// router.put('/statuses', db.updateStatus);
+// router.delete('/statuses/:id', db.deleteStatus);
+//
+// router.get('/likes/:id', db.getUserLikes);
+// router.post('/likes/:id', db.createUserLike);
+// router.delete('/likes/:id', db.deleteLike);
+
+module.exports = {
+    getUsers: getUsers,
+    getUser: getUser
+}
--- routes.js
+++ routes.js
@@ -2,27 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.send('It worked!');
-  console.log('It worked!');
+  res.send('Welcome to the Twitter clone api');
 });
- 
+
 var db = require('./queries');
 
 router.get('/users', db.getUsers);
 router.get('/users/:id', db.getUser);
-router.post('/users', db.createUser);
-router.put('/users', db.updateUser);
-router.delete('/users/:id', db.deleteUser);
+// router.post('/users', db.createUser);
+// router.put('/users', db.updateUser);
+// router.delete('/users/:id', db.deleteUser);
 
 
-router.get('/statuses', db.getStatuses);
-router.get('/statuses/:id', db.getStatus);
-router.post('/statuses', db.createStatus);
-router.put('/statuses', db.updateStatus);
-router.delete('/statuses/:id', db.deleteStatus);
-
-router.get('/likes/:id', db.getUserLikes);
-router.post('/likes/:id', db.createUserLike);
-router.delete('/likes/:id', db.deleteLike);
+// router.get('/statuses', db.getStatuses);
+// router.get('/statuses/:id', db.getStatus);
+// router.post('/statuses', db.createStatus);
+// router.put('/statuses', db.updateStatus);
+// router.delete('/statuses/:id', db.deleteStatus);
+//
+// router.get('/likes/:id', db.getUserLikes);
+// router.post('/likes/:id', db.createUserLike);
+// router.delete('/likes/:id', db.deleteLike);
 
 module.exports = router;
