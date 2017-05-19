2d558431492ec9f355adb121fbc1456fad86ef1f
Jonathan Pinlac <jonppinlac@gmail.com>
2017-05-19 16:02:58 -0400

    Added connection instructions

--- db.js
+++ db.js
@@ -1,3 +1,14 @@
+// Cloud sql start proxy
+// sudo mkdir /cloudsql; sudo chmod 777 /cloudsql
+// ./cloud_sql_proxy -dir=/cloudsql -instances=airnetp:us-central1:airnet-sql \ -credential_file=./airnetP-fe0b5224641b.json &
+// or ./cloud_sql_proxy -dir=/cloudsql &
+//
+// to connect using psql:
+// psql -U postgres -h /cloudsql/airnetp:us-central1:airnet-sql
+// or psql "sslmode=disable host=/cloudsql/airnetp:us-central1:airnet-sql user=postgres"
+//
+// credential file name = airnetP-fe0b5224641b.json
+
 const pg = require('pg');
 
 var config = {
